23a97d7732d5aeba3ffe8baaa5b8dea8aea833ad
roth123-del <Cheatchet108@gamil.com>
2025-06-28 18:06:13 +0700

    first

--- java.js
+++ java.js
@@ -5,7 +5,7 @@ const text = document.querySelector(".text");
 btnAgree.addEventListener("click", () => {
     text.innerHTML = "I love you so much...";
 });
-btnDisagree.addEventListener("click", () => {
+btnAgree.addEventListener("click", () => {
     gif.src = "https://i.pinimg.com/originals/ef/c7/8f/efc78ff81b7ad093295169571cd511f1.gif"
 });
 btnDisagree.addEventListener("click", () => {
